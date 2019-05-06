cfa182f0d1938edd4aed89b52b789e43a3d8fb1a
elzieconnor <31282129+elzieconnor@users.noreply.github.com>
2019-05-06 13:11:15 -0500

    Update Game.java

--- src/main/Game.java
+++ src/main/Game.java
@@ -4,6 +4,7 @@ import javafx.scene.paint.Color;
 
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 
 public class Game extends Canvas implements Runnable {
@@ -11,8 +12,8 @@ public class Game extends Canvas implements Runnable {
     private boolean isRunning = false;
     private Thread thread;
     private ObjectHandler handler;
-
-    private BufferedImage map = null;
+    
+    private BufferedImage level = null;
 
     public Game(){
         new Window(1000, 700, "ULTRAVIOLET", this);
@@ -20,12 +21,12 @@ public class Game extends Canvas implements Runnable {
 
         handler = new ObjectHandler();
         this.addKeyListener(new KeyInput(handler));
+        
+        BufferedImageLoader loader = new BufferedImageLoader();
+        level = loader.loadImage("/minorMaze.png");
 
-        ImageLoader loader = new ImageLoader();
-        map = loader.loadImage("/minorMaze.png");
+        handler.addObject(new Player(400, 500, ID.Player, handler));
 
-        //handler.addObject(new Player(400, 500, ID.Player, handler));
-        loadMap(map);
     }
 
     private void start(){
@@ -80,7 +81,7 @@ public class Game extends Canvas implements Runnable {
     }
 
     public void render(){
-        //Toolkit.getDefaultToolkit().sync();
+        Toolkit.getDefaultToolkit().sync();
         BufferStrategy bs = this.getBufferStrategy();
         if(bs == null){
             this.createBufferStrategy(3);
@@ -88,39 +89,16 @@ public class Game extends Canvas implements Runnable {
         }
 
         Graphics g = bs.getDrawGraphics();
+        Toolkit.getDefaultToolkit().sync();
 
-        //Toolkit.getDefaultToolkit().sync();
-
-        //g.setColor(java.awt.Color.BLACK);
-        //g.fillRect(0, 0, 1000, 700);
+        g.setColor(java.awt.Color.BLACK);
+        g.fillRect(0, 0, 1000, 700);
 
         handler.render(g);
 
         g.dispose();
         bs.show();
-    }
 
-    private void loadMap(BufferedImage image) {
-        int w = image.getWidth();
-        int h = image.getHeight();
-
-        for (int xx = 0; xx < w; xx++) {
-            for (int yy = 0; yy < h; yy++) {
-                int pixel = image.getRGB(xx, yy);
-                int red = (pixel >> 16) & 0xff;
-                // We don't use green but need to move through the byte to get blue
-                int green = (pixel >> 8) & 0xff;
-                int blue = (pixel) & 0xff;
-
-                // We use rgb(131, 0, 139) for ultraviolet but we can just check the red value
-                if (red == 255 & green == 255 & blue == 255) {
-                    handler.addObject(new Block(xx*32, yy*32, ID.Block));
-                }
-                if (red == 0 && green == 0 && blue == 255) {
-                    handler.addObject(new Player(xx*32, yy*32, ID.Player, handler));
-                }
-            }
-        }
     }
 
     public static void main(String args[]){
